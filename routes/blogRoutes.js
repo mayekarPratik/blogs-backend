035464986e516e97c2f588cc9c4e5d937f1981ce
Pratik Mayekar <pratikmayekar3@gmail.com>
2022-07-03 03:57:09 +0530

    added basic crud operations api

---
+++ routes/blogRoutes.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const Blogs = require("./../models/blogs");
+const router = express.Router();
+
+// Getting all
+router.get("/", async (req, res) => {
+  try {
+    const blogsData = await Blogs.find();
+    if (!blogsData) {
+      res.status(200).json({ message: "No Blogs Found" });
+    }
+    res.status(200).json({ message: "Success", data: blogsData });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Getting One
+router.get("/:id", getBlogDetail, (req, res) => {
+  res.status(200).json({ message: "Successfully fetch", data: res.blog });
+});
+
+// Creating one
+router.post("/", async (req, res) => {
+  const blog = new Blogs({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const newBlog = await blog.save();
+    res.status(200).json({ message: "Successfully created", data: newBlog });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Updating One
+router.patch("/:id", getBlogDetail, async (req, res) => {
+  if (req.body.title != null) {
+    res.blog.title = req.body.title;
+  }
+  if (req.body.description != null) {
+    res.blog.description = req.body.description;
+  }
+  try {
+    const updatedBlog = await res.blog.save();
+    res
+      .status(200)
+      .json({ message: "Successfully Updated", data: updatedBlog });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Deleting One
+router.delete("/:id", getBlogDetail, async (req, res) => {
+  try {
+    await res.blog.remove();
+    res.json({ message: "Deleted Blog" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getBlogDetail(req, res, next) {
+  let blog;
+  try {
+    blog = await Blogs.findById(req.params.id);
+    if (blog == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.blog = blog;
+  next();
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const blogsRouter = require("./routes/blogRoutes");
 
-const app = express()
+// Set up the server
+const app = express();
+app.use(express.json());
 
-app.get('/', (req,res) =>{
-    res.send("Hello World")
-})
+// Connect to MongoDB Atlas
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.listen(5000)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.on("open", () => console.log("Connected to MongoDB Atlas"));
 
+// Check server status
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server running",
+  });
+});
 
+// Connect server to routes
+app.use("/blogs", blogsRouter);
+
+// Listen on specified port or port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
